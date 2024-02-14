91b9aeb06e4ef5f34675071393cc4a42d1a4c058
Mauricio_del_Valle <mauriciodelvallefarrera@gmail.com>
2024-02-13 19:12:34 -0600

    Importacion de Librerias GOOGLE

--- app/src/main/java/com/example/conexionapi/MainActivity.java
+++ app/src/main/java/com/example/conexionapi/MainActivity.java
@@ -8,6 +8,15 @@ import com.google.android.gms.wearable.CapabilityClient;
 import com.google.android.gms.wearable.CapabilityInfo;
 import com.google.android.gms.wearable.DataClient;
 import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataItem;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.MessageClient;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
 
 import com.google.android.gms.wearable.MessageClient;
 
