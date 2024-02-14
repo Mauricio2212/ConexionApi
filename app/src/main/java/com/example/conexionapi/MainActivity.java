e6826ac433c9fcccf83586c7a21dd061626bcab6
Mauricio_del_Valle <mauriciodelvallefarrera@gmail.com>
2024-02-13 19:39:07 -0600

    Funciones onClick / sendStuff

--- app/src/main/java/com/example/conexionapi/MainActivity.java
+++ app/src/main/java/com/example/conexionapi/MainActivity.java
@@ -7,7 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.gms.wearable.CapabilityClient;
 import com.google.android.gms.wearable.CapabilityInfo;
 import com.google.android.gms.wearable.DataClient;
@@ -45,6 +48,29 @@ public class MainActivity extends AppCompatActivity implements
         textoEnviar = findViewById(R.id.txtEnviar);
         btnEnviar = findViewById(R.id.btnEnviar);
 
-        
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendStuff(textoEnviar.getText().toString());
+            }
+        });
+    }
+
+    private void sendStuff(String datos) {
+        mDataClient = Wearable.getDataClient(this);
+        PutDataMapRequest putDataMapReq = PutDataMapRequest.create("/ messageMobile").setUrgent();
+        // putDataMapReq.getDataMap().putInt(COUNT_KEY, 100);
+        putDataMapReq.getDataMap().putString(COUNT_KEY2, datos);
+        PutDataRequest putDataReq = putDataMapReq.asPutDataRequest().setUrgent();
+        Task<DataItem> putDataTask = mDataClient.putDataItem(putDataReq);
+
+        putDataTask.addOnSuccessListener(new OnSuccessListener<DataItem>()
+        {
+            @Override
+            public void onSuccess(DataItem dataItem) {
+                Toast.makeText(getApplicationContext(), "eviado", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
