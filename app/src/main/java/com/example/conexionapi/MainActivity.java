eaa3e680513195bc419b56a555177c5ab9b2a404
Mauricio_del_Valle <mauriciodelvallefarrera@gmail.com>
2024-02-13 21:13:46 -0600

    Interfaz gráfica MainActivity

--- app/src/main/java/com/example/conexionapi/MainActivity.java
+++ app/src/main/java/com/example/conexionapi/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.conexionapi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -39,6 +40,7 @@ public class MainActivity extends AppCompatActivity implements
     EditText textoEnviar;
     Button btnEnviar;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
